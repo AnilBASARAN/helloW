e59a94d6c718ad32658fd42c817f42d911220e2a
AnilBASARAN <nlbsrn@gmail.com>
2025-04-08 05:27:51 +0300

    changed to make it cleaner

--- main.go
+++ main.go
@@ -2,24 +2,15 @@ package main
 
 import (
 
-	"log"
 )
 // also fmt can be used too instead of log, its a valid approach also when it comes to console.log
 func main() {
-	var myString string
-	myString = "Green"
-	log.Println("myString is set to", myString)
 
-	changeUsingPointer(&myString)
-
-	log.Println("second time :",myString)
 
 }
 
-func changeUsingPointer(s *string){
-	// this is a different way than usual,  ":=" this is declaring and equaling to the variable at the same time
-	newValue := "Red"
-	*s = newValue
+func saySomething(s string) (string,string) {
+	return s, "World"
 }
 
 // pointer is , it points to a spesific location in memory and gives you a means of getting that particular location in memory
