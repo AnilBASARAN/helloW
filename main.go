8a929455ed522273e7ab0791e9b48288bb8f5adc
AnilBASARAN <nlbsrn@gmail.com>
2025-04-09 06:43:02 +0300

    do some calculations as well

--- main.go
+++ main.go
@@ -5,19 +5,30 @@ import (
 	"net/http"
 )
 
-type myStruct struct {
-	FirstName string
-}
-
-func main() {
-
-	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request){
-		n, err := fmt.Fprintf(w, "hello world")
+func Home(w http.ResponseWriter, r * http.Request){
+		n, err := fmt.Fprintf(w, "This is the Home Page")
 		if err != nil {
 			fmt.Println(err)
 		}
 		fmt.Println(fmt.Sprintf("Number of Bytes written: %d ", n ))
-	})
+	
+}
+func About(w http.ResponseWriter, r * http.Request){
+	sum := AddValues(2,2)
+
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum ))
+
+}
+
+func AddValues(x,y int ) int {
+
+	return x + y
+}
+
+func main() {
+
+	http.HandleFunc("/",Home)
+	http.HandleFunc("/about",About)
 
 	_ = http.ListenAndServe(":8080",nil)
 }
