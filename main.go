462c10bb4ffb8a1c243d93ed9faccd03d89b6860
AnilBASARAN <nlbsrn@gmail.com>
2025-04-09 23:16:03 +0300

    added a functionality to call out what port is used when server is running on

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8080"
+
 func Home(w http.ResponseWriter, r * http.Request){
 		n, err := fmt.Fprintf(w, "This is the Home Page")
 		if err != nil {
@@ -14,13 +16,13 @@ func Home(w http.ResponseWriter, r * http.Request){
 	
 }
 func About(w http.ResponseWriter, r * http.Request){
-	sum := AddValues(2,2)
+	sum := addValues(2,2)
 
 	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum ))
 
 }
 
-func AddValues(x,y int ) int {
+func addValues(x,y int ) int {
 
 	return x + y
 }
@@ -30,7 +32,8 @@ func main() {
 	http.HandleFunc("/",Home)
 	http.HandleFunc("/about",About)
 
-	_ = http.ListenAndServe(":8080",nil)
+	fmt.Println(fmt.Sprintf("Starting application on port %s",portNumber))
+	_ = http.ListenAndServe(portNumber,nil)
 }
 
 
