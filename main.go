a30b488e6507d36b894578c482bc96adca5eef79
AnilBASARAN <nlbsrn@gmail.com>
2025-04-10 00:25:55 +0300

    now we have 3 pages, one of which is divide and we use a function to divide hard coded numbers

--- main.go
+++ main.go
@@ -29,7 +29,11 @@ func addValues(x,y int ) int {
 }
 
 func Divide(w http.ResponseWriter, r *http.Request) {
-
+	f, err := divideValues(100.0,10.0)
+	if err != nil {
+		fmt.Fprintf(w,"Can not divide by 0")
+	}
+	fmt.Fprintf(w,fmt.Sprintf("%f divided by %f is %f",100.0,10.0,f))
 }
 
 func divideValues(x,y float32) (float32 , error){
@@ -45,6 +49,7 @@ func main() {
 
 	http.HandleFunc("/",Home)
 	http.HandleFunc("/about",About)
+	http.HandleFunc("/divide",Divide)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s",portNumber))
 	_ = http.ListenAndServe(portNumber,nil)
