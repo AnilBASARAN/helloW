a067457e4d73476de559d739e3d53ce200cd2510
AnilBASARAN <nlbsrn@gmail.com>
2025-04-10 01:18:50 +0300

    we created the about and home pages with tmpl extensions

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 )
@@ -23,34 +22,12 @@ func About(w http.ResponseWriter, r * http.Request){
 
 }
 
-func addValues(x,y int ) int {
-
-	return x + y
-}
-
-func Divide(w http.ResponseWriter, r *http.Request) {
-	f, err := divideValues(100.0,0.0)
-	if err != nil {
-		fmt.Fprintf(w,"Can not divide by 0")
-		return
-	}
-	fmt.Fprintf(w,fmt.Sprintf("%f divided by %f is %f",100.0,0.0,f))
-}
-
-func divideValues(x,y float32) (float32 , error){
-	if y <= 0 {
-		err := errors.New("can not divided by zero")
-		return 0, err
-	}
-	result := x /y
-	return result, nil
-}
 
 func main() {
 
 	http.HandleFunc("/",Home)
 	http.HandleFunc("/about",About)
-	http.HandleFunc("/divide",Divide)
+
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s",portNumber))
 	_ = http.ListenAndServe(portNumber,nil)
