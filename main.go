ae8d3c2f7c7b3446b36ba1b37ab3f513e6be1800
AnilBASARAN <nlbsrn@gmail.com>
2025-04-10 00:27:58 +0300

    added the return

--- main.go
+++ main.go
@@ -29,11 +29,12 @@ func addValues(x,y int ) int {
 }
 
 func Divide(w http.ResponseWriter, r *http.Request) {
-	f, err := divideValues(100.0,10.0)
+	f, err := divideValues(100.0,0.0)
 	if err != nil {
 		fmt.Fprintf(w,"Can not divide by 0")
+		return
 	}
-	fmt.Fprintf(w,fmt.Sprintf("%f divided by %f is %f",100.0,10.0,f))
+	fmt.Fprintf(w,fmt.Sprintf("%f divided by %f is %f",100.0,0.0,f))
 }
 
 func divideValues(x,y float32) (float32 , error){
