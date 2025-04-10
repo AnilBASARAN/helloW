50f48513ff270fd559d6fe44737a7b39e1bf14b9
AnilBASARAN <nlbsrn@gmail.com>
2025-04-10 06:35:23 +0300

    created different folders for handlers

--- main.go
+++ main.go
@@ -3,38 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"text/template"
+	"myapp/handlers"
 )
 
 const portNumber = ":8080"
 
-func Home(w http.ResponseWriter, r * http.Request){
-	renderTemplate(w, "home.page.tmpl")
-	
-}
-func About(w http.ResponseWriter, r * http.Request){
-	renderTemplate(w, "about.page.tmpl")
-}
+
 
 
 func main() {
 
-	http.HandleFunc("/",Home)
-	http.HandleFunc("/about",About)
-
+	http.HandleFunc("/",handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+	
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s",portNumber))
 	_ = http.ListenAndServe(portNumber,nil)
 }
 
-func renderTemplate(w http.ResponseWriter, tmpl string){
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w,nil)
-	if err != nil {
-		fmt.Println("error parsing template:",err)
-		return
-	}
-}
+
 
 
 
