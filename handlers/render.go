8ba22d96ce336b44340d158baecfe20cc3624cfe
AnilBASARAN <nlbsrn@gmail.com>
2025-04-11 00:42:25 +0300

    its all in different folders and packages and it works

---
+++ handlers/render.go
@@ -0,0 +1,16 @@
+package handlers
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func renderTemplate(w http.ResponseWriter, tmpl string){
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w,nil)
+	if err != nil {
+		fmt.Println("error parsing template:",err)
+		return
+	}
+}
