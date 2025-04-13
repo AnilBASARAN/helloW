522169f78f5f11bb69fc7a1717c297afcde0dd10
AnilBASARAN <nlbsrn@gmail.com>
2025-04-13 11:21:03 +0300

    all folders created

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,12 +1,12 @@
-package handlers
+package render
 
 import (
 	"fmt"
 	"html/template"
 	"net/http"
 )
-
-func renderTemplate(w http.ResponseWriter, tmpl string){
+// RenderTemplate renders templates using html/template
+func RenderTemplate(w http.ResponseWriter, tmpl string){
 	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
 	err := parsedTemplate.Execute(w,nil)
 	if err != nil {
