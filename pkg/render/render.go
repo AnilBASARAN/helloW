8202327fbfeaa25382fa4b1b7be6ee6b54fe22fe
AnilBASARAN <nlbsrn@gmail.com>
2025-04-13 11:52:57 +0300

    "path/filepath" // add this to make all work

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -4,13 +4,25 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"path/filepath"
 )
+
 // RenderTemplate renders templates using html/template
-func RenderTemplate(w http.ResponseWriter, tmpl string){
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w,nil)
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	basePath := filepath.Join("templates", "base.layout.tmpl")
+	pagePath := filepath.Join("templates", tmpl)
+
+	parsedTemplate, err := template.ParseFiles(basePath, pagePath)
 	if err != nil {
-		fmt.Println("error parsing template:",err)
+		fmt.Println("error parsing templates:", err)
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = parsedTemplate.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		fmt.Println("error executing template:", err)
+		http.Error(w, "Internal Server Error", 500)
 		return
 	}
 }
